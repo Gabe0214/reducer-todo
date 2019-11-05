b438b627344f123205fa7e48f7d6fd6e571b5b0b
Gabriel Anguiano <anguianog97@gmail.com>
2019-11-05 15:39:46 -0800

    added Todo component

---
+++ reducer-app/src/components/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Todo = props => {
+    return (
+        <div>
+            <h2>{props.todo.item}</h2>
+            
+        </div>
+    )
+}
+
+export default Todo; 
